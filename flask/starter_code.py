2a0926d997de06d941ddb67575c185d1ad712cbf
EdwardFine <edfine03@hotmail.com>
2023-05-04 10:22:31 -0700

    Starter Code

---
+++ flask/starter_code.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "This is the Home Page!"
+
+@app.route('/hello/<name>')
+def name(name):
+    return f"Hello, {name}"
+
+if __name__=="__main__":
+    app.run(debug=True,port=5001)
